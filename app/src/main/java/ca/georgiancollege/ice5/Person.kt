9289d3ec052dcd0e62b9a3426ef88a3cfeca3b21
sashiacharya <156461925+sashiacharya@users.noreply.github.com>
2025-06-04 00:33:40 -0400

    created person.kt

--- app/src/main/java/ca/georgiancollege/ice5/Person.kt
+++ app/src/main/java/ca/georgiancollege/ice5/Person.kt
@@ -1,4 +1,16 @@
 package ca.georgiancollege.ice5
 
-class Person {
+class Person(private var name: String, private var age: Float)
+{
+    // Properties with custom getters and setters
+    var Name: String
+        get() = name
+        set(value) {
+            if (value.isNotBlank()) name = value
+        }
+    var Age: Float
+        get() = age
+        set(value) {
+            if (value >= 0) age = value
+        }
 }
